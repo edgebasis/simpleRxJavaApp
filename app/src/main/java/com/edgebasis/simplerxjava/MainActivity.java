c65dca7adacb76f9ac9576ac073dcec62cd38f15
Mujtaba Mahmood <mujtabamahmood@Mujtabas-MacBook-Pro.local>
2018-04-02 21:41:12 -0400

    updates commit

--- app/src/main/java/com/edgebasis/simplerxjava/MainActivity.java
+++ app/src/main/java/com/edgebasis/simplerxjava/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.util.Observable;
+import java.util.concurrent.TimeUnit;
 
 import rx.Subscriber;
 
@@ -25,12 +26,60 @@ public class MainActivity extends AppCompatActivity {
         btnRun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                testObservable();
+                testObservableInterval();
             }
         });
     }
 
-    private void testObservable() {
+    private void testObservableInterval() {
+        rx.Observable.interval(2, TimeUnit.SECONDS).subscribe(new Subscriber<Long>() {
+
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                if(aLong==5)
+                    unsubscribe();
+                Log.i("onNext interv", String.valueOf(aLong));
+            }
+        });
+
+    }
+
+    private void testObservableJust() {
+        rx.Observable.just(1,2,3,4,5,6,7,8,9,10).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.i("onNext Just", String.valueOf(integer));
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void testObservableFrom() {
         rx.Observable.from(new Integer[]{1,2,3}).subscribe(new Subscriber<Integer>() {
             @Override
             public void onCompleted() {
